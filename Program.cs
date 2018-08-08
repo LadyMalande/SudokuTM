b0d34032d87b3a32aea64a18afee9943aed531e5
LadyMalande <42152093+LadyMalande@users.noreply.github.com>
2018-08-08 15:09:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudokuTM
+{
+    /// <summary>
+    /// Hlavní třída, ze které se volá spuštění programu.
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// Hlavní vstupní bod aplikace. Zavolá Form1 (Menu).
+        /// </summary>
+        [STAThread]
+        
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainMenu());
+        }
+    }
+}
